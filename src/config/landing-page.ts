188dcf9d268977e08c5ff87c61d3b7b6abe1a49f
Ajay Kumar Maharana <ajaymaharana9@gmail.com>
2025-07-17 11:19:01 +0000

    i want to change from this
    export const landingPageConfig = {
      carous

--- src/config/landing-page.ts
+++ src/config/landing-page.ts
@@ -1,10 +1,10 @@
 
 export const landingPageConfig = {
   carouselImages: [
-    { src: "https://placehold.co/600x400.png", alt: "Indian thali", hint: "indian thali" },
-    { src: "https://placehold.co/600x400.png", alt: "Pasta bowl", hint: "pasta bowl" },
-    { src: "https://placehold.co/600x400.png", alt: "Healthy salad", hint: "healthy salad" },
-    { src: "https://placehold.co/600x400.png", alt: "Brunch platter", hint: "brunch platter" },
-    { src: "https://placehold.co/600x400.png", alt: "Veggie stir-fry", hint: "veggie stir-fry" },
+    { src: "https://www.nicepng.com/png/full/12-127001_the-best-thali-indian-veg-thali.png", alt: "Indian thali", hint: "indian thali" },
+    { src: "https://www.virginmediatelevision.ie/content//content/images/1310/298405-Nduja-Mascarpone-Pasta-1739549481-rectangular_image_1280x720.png", alt: "Pasta bowl", hint: "pasta bowl" },
+    { src: "https://png.pngtree.com/png-clipart/20241121/original/pngtree-salad-png-image_17276894.png", alt: "Healthy salad", hint: "healthy salad" },
+    { src: "https://png.pngtree.com/png-vector/20250215/ourmid/pngtree-a-beautifully-arranged-breakfast-and-brunch-food-platter-png-image_15482669.png", alt: "Brunch platter", hint: "brunch platter" },
+    { src: "https://static.vecteezy.com/system/resources/previews/060/839/932/non_2x/plate-with-stir-fried-vegetables-isolated-on-transparent-background-top-view-png.png", alt: "Veggie stir-fry", hint: "veggie stir-fry" },
   ]
 };
